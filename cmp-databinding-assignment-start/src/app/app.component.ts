f0351b343a77c2361976b9f965cf86197a8a16ee
Kishore <harishkishore14122000@gmail.com>
2024-02-27 11:53:50 +0530

    new tutorial

---
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[]= [];
+
+  onintervalFired(emittedNumber: number) {
+    if (emittedNumber % 2 === 0) {
+      this.evenNumbers.push(emittedNumber);
+    } else {
+      this.oddNumbers.push(emittedNumber);
+    }
+    console.log(emittedNumber);
+  }
+}
