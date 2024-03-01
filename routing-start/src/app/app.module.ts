d6ae754a85778ddfc74aad8e105581e0212d5abc
harishkishore14 <harishkishore14122000@gmail.com>
2024-03-01 19:09:59 +0530

    Redirecting & wildcard routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
+import { RouterModule, Routes } from '@angular/router';
+
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -10,10 +13,10 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
 import { ServersService } from './servers/servers.service';
-
-
-import { RouterModule, Routes } from '@angular/router';
 
 
 const appRoutes: Routes = [
@@ -25,6 +28,8 @@ const appRoutes: Routes = [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
   ] },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found', pathMatch: 'full' }
 ];
 
 @NgModule({
@@ -35,7 +40,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
