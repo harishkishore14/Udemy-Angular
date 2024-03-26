aa061ba961b8e3e7bb39bef00a346efe33335c36
Kishore <kishore.g@samco.in>
2024-03-26 12:11:25 +0530

    Understanding RxJS (Built-in) Operators

--- observable-start/src/app/home/home.component.ts
+++ observable-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription, interval } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -30,7 +31,11 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000)
     });
 
-    this.firstObsSubscription = customInterval.subscribe(data => {
+    this.firstObsSubscription = customInterval.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return "Round: " + (data + 1);
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
